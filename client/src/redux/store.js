afd7422540ecc3fd4e9e8e0ffa7a1df67344a46c
shuklasiddhi225 <shuklasiddhi225@gmail.com>
2024-11-21 20:44:19 +0530

    create and add OAuth component

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,21 +1,24 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice'
-import {persistReducer,persistStore} from 'redux-persist';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-const rootReducer=combineReducers({user:userReducer})
-const persistConfig={
-    keys:'root',
-    storage,
-    version:1,
 
-}
+const rootReducer = combineReducers({ user: userReducer });
 
-const persistedReducer=persistReducer(persistConfig,rootReducer)
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware)=>getDefaultMiddleware({
-    serializableCheck:false,
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
-export const persistor=persistStore(store);
 
+export const persistor = persistStore(store);
